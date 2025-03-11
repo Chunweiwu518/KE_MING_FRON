d9005793bf2e1c55f611f3db133e6900075a44af
wuchunwei <wuchunwei0518@gmail.com>
2025-03-11 21:07:26 +0800

    Revise

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'ke-ming-fron.onrender.com',
+      '.onrender.com', // 這會允許所有 onrender.com 子域名
+    ],
+  },
 })
